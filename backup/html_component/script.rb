c57513bf9931a77d3dabc0afff8ecd4cdf6d95f7
adelinelim <adelineyiing@gmail.com>
2016-08-22 18:49:28 +0800

    clean up

--- backup/html_component/script.rb
+++
@@ -1,7 +0,0 @@
-module HtmlComponent
-  class Script < ComponentTag
-    def tag_name
-      "script"
-    end
-  end
-end
--- backup/html_componet.rb
+++
@@ -1,5 +0,0 @@
-# class html_component
-#
-# end
-#
-# require_dir(:html_component, __FILE__)
